f2c8fb64011f8be5f42c01baf18ba26fb2fc1089
kag <kag@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-05 14:23:27 +0000

    Those who simply updated the plugin without going in the admin panel to select thumbnail sizes weren't seeing their album thumbnails anymore
    git-svn-id: https://plugins.svn.wordpress.org/kpicasa-gallery/tags/0.1.7@107876 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kpg.class.php
@@ -0,0 +1,513 @@
+<?php
+
+if ( !class_exists('KPicasaGallery') )
+{
+	class KPicasaGallery
+	{
+		private $username;
+		private $picEngine;
+		private $showOnlyAlbums;
+		private $cacheTimeout;
+
+		private $nbAlbumsPerPage;
+		private $nbAlbumsPerRow;
+		private $albumThumbSize;
+
+		private $nbPhotosPerPage;
+		private $nbPhotosPerRow;
+		private $photoThumbSize;
+
+		public function __construct($username = null, $showOnlyAlbums)
+		{
+			$this->username        = $username != null ? $username : get_option( 'kpg_username' );
+			$this->picEngine       = get_option( 'kpg_picEngine' );
+			$this->showOnlyAlbums  = is_array( $showOnlyAlbums ) ? $showOnlyAlbums : array();
+			$this->cacheTimeout    = 60 * 60 * 1;
+
+			$this->nbAlbumsPerPage = get_option( 'kpg_albumPerPage' );
+			$this->nbAlbumsPerRow  = get_option( 'kpg_albumPerRow' );
+			$this->albumThumbSize  = get_option( 'kpg_albumThumbSize' );
+
+			$this->nbPhotosPerPage = get_option( 'kpg_photoPerPage' );
+			$this->nbPhotosPerRow  = get_option( 'kpg_photoPerRow' );
+			$this->photoThumbSize  = get_option( 'kpg_photoThumbSize' );
+
+			if ( !strlen( $this->username ) )
+			{
+				if ( $this->checkError( new WP_Error('kpicasa_gallery-username-required', "<strong>Error:</strong> you must go to the admin section and set your Picasa Web Album Username in the Options section.") ) )
+				{
+					return false;
+				}
+			}
+
+			// Set default values, if necessary
+			if ( intval( $this->nbAlbumsPerPage ) < 0 )
+			{
+				$this->nbAlbumsPerPage = 0;
+			}
+			if ( intval( $this->nbAlbumsPerRow ) < 1 )
+			{
+				$this->nbAlbumsPerRow = 1;
+			}
+			if ( intval( $this->nbPhotosPerPage ) < 0 )
+			{
+				$this->nbPhotosPerPage = 0;
+			}
+			if ( intval( $this->nbPhotosPerRow ) < 1 )
+			{
+				$this->nbPhotosPerRow = 2;
+			}
+
+			if ( count($this->showOnlyAlbums) == 1 || (isset($_GET['album']) && strlen($_GET['album'])) )
+			{
+				if ( count($this->showOnlyAlbums) == 1 )
+				{
+					$tmp     = explode('#', $this->showOnlyAlbums[0]);
+					$album   = $tmp[0];
+					$direct  = true;
+					$authKey = isset($tmp[1]) ? $tmp[1] : '';
+				}
+				else
+				{
+					$album   = $_GET['album'];
+					$direct  = false;
+					$authKey = '';
+				}
+
+				if ( $this->checkError( $this->displayPictures($album, $direct, $authKey) ) )
+				{
+					return false;
+				}
+			}
+			else
+			{
+				if ( $this->checkError( $this->displayAlbums() ) )
+				{
+					return false;
+				}
+			}
+		}
+
+		private function displayAlbums()
+		{
+			//----------------------------------------
+			// Get the XML
+			//----------------------------------------
+			$data = wp_cache_get('kPicasaGallery', 'kPicasaGallery');
+			if ( false === $data )
+			{
+				$url  = "http://picasaweb.google.com/data/feed/api/user/".urlencode($this->username)."?kind=album";
+				$data = $this->fetch($url);
+				if ( is_wp_error($data) )
+				{
+					return $data;
+				}
+				$data = str_replace('gphoto:', 'gphoto_', $data);
+				$data = str_replace('media:', 'media_', $data);
+				wp_cache_set('kPicasaGallery', $data, 'kPicasaGallery', $this->cacheTimeout);
+			}
+			$xml = @simplexml_load_string($data);
+			if ( $xml === false )
+			{
+				return new WP_Error( 'kpicasa_gallery-invalid-response', "<strong>Error:</strong> the communication with Picasa Web Albums didn't go as expected. Here's what Picasa Web Albums said:<br /><br />".$data );
+			}
+
+			//----------------------------------------
+			// Prepare some variables
+			//----------------------------------------
+			$page = isset($_GET['kpgp']) && intval($_GET['kpgp']) > 1 ? intval($_GET['kpgp']) : 1; // kpgp = kPicasa Gallery Page
+
+			$url = get_permalink();
+			if ( $page > 1 )
+			{
+				$url = add_query_arg('kpgp', $page, $url);
+			}
+
+			if ( $this->nbAlbumsPerPage > 0 )
+			{
+				$start = ($page - 1) * $this->nbAlbumsPerPage;
+				$stop  = $start + $this->nbAlbumsPerPage - 1;
+			}
+			else
+			{
+				$start = 0;
+				$stop  = count( $xml->entry ) - 1;
+			}
+
+			// Set the class, depending on how many albums per row
+			$class = $this->nbAlbumsPerRow == 1 ? 'kpg-thumb-onePerRow' : 'kpg-thumb-multiplePerRow';
+
+			//----------------------------------------
+			// Loop through the albums
+			//----------------------------------------
+			print '<table cellpadding="0" cellspacing="0" border="0" width="100%" id="kpg-albums">';
+			$i = 0; $j = 0;
+			foreach( $xml->entry as $album )
+			{
+				if ($i >= $start && $i <= $stop &&
+				( !count($this->showOnlyAlbums) || in_array((string) $album->gphoto_name, $this->showOnlyAlbums) ))
+				{
+					if ( $j % $this->nbAlbumsPerRow == 0 )
+					{
+						$remainingWidth = 100;
+						if ($j > 0)
+						{
+							print '</tr>';
+						}
+						print '<tr>';
+					}
+
+					// if last cell of the row, simply put remaining width
+					$width = ( ($j+1) % $this->nbAlbumsPerRow == 0 ) ? $remainingWidth : round( 100 / $this->nbAlbumsPerRow );
+					$remainingWidth -= $width;
+					print "<td width='$width%'>";
+
+					$name      = (string) $album->gphoto_name;
+					$title     = wp_specialchars( (string) $album->title );
+					$summary   = wp_specialchars( (string) $album->summary );
+					$location  = wp_specialchars( (string) $album->gphoto_location );
+					$nbPhotos  = (string) $album->gphoto_numphotos;
+					$albumURL  = add_query_arg('album', $name, $url);
+					$thumbURL  = (string) $album->media_group->media_thumbnail['url'];
+					$thumbH    = (string) $album->media_group->media_thumbnail['height'];
+					$thumbW    = (string) $album->media_group->media_thumbnail['width'];
+
+					if ( $this->albumThumbSize != false && $this->albumThumbSize != 160 )
+					{
+						$thumbURL = str_replace('/s160-c/', '/s'.$this->albumThumbSize.'-c/', $thumbURL);
+						$thumbH   = round( ($this->albumThumbSize / $thumbH) * $thumbH );
+						$thumbW   = round( ($this->albumThumbSize / $thumbW) * $thumbW );
+					}
+
+					print "<a href='$albumURL'><img src='$thumbURL' height='$thumbH' width='$thumbW' alt='".str_replace("'", "&#39;", $title)."' class='kpg-thumb $class' /></a>";
+					print "<div class='kpg-title'><a href='$albumURL'>$title</a></div>";
+					if ( strlen($summary) )
+					{
+						print "<div class='kpg-summary'>$summary</div>";
+					}
+					if ( strlen($location) )
+					{
+						print "<div class='kpg-location'>$location</div>";
+					}
+					print "<div class='kpg-nbPhotos'>$nbPhotos photo".($nbPhotos > 1 ? 's' : '').'</div>';
+					print '</td>';
+					$j++;
+				}
+				$i++;
+			}
+
+			// never leave the last row with insuficient cells
+			if ($this->nbPhotosPerRow > 0)
+			{
+				while ($j % $this->nbPhotosPerRow > 0)
+				{
+					print '<td>&nbsp;</td>';
+					$j++;
+				}
+			}
+
+			print '</tr>';
+			print '</table>';
+			print '<div style="clear: both;" />&nbsp;</div>';
+
+			//----------------------------------------
+			// Paginator
+			//----------------------------------------
+			$nbItems = count($this->showOnlyAlbums) > 0 ? count($this->showOnlyAlbums) : count($xml->entry);
+			$this->paginator( $page, 'kpgp', $this->nbAlbumsPerPage, $nbItems );
+			return true;
+		}
+
+		private function displayPictures($album, $direct = false, $authKey = '')
+		{
+			//----------------------------------------
+			// Get the XML
+			//----------------------------------------
+			$data = wp_cache_get('kPicasaGallery_'.$album, 'kPicasaGallery');
+			if ( false === $data )
+			{
+				$url = "http://picasaweb.google.com/data/feed/api/user/".urlencode($this->username)."/album/".urlencode($album)."?kind=photo";
+				if ( strlen($authKey) > 0 )
+				{
+					$url .= '&authkey='.$authKey;
+				}
+				$data = $this->fetch($url);
+				if ( is_wp_error($data) )
+				{
+					return $data;
+				}
+				$data = str_replace('gphoto:', 'gphoto_', $data);
+				$data = str_replace('media:', 'media_', $data);
+				wp_cache_set('kPicasaGallery_'.$album, $data, 'kPicasaGallery', $this->cacheTimeout);
+			}
+			$xml = @simplexml_load_string($data);
+			if ( $xml === false )
+			{
+				return new WP_Error( 'kpicasa_gallery-invalid-response', __("<strong>Error:</strong> the communication with Picasa Web Albums didn't go as expected. Here's what Picasa Web Albums said:<br /><br /> ".$data) );
+			}
+
+			//----------------------------------------
+			// Display "back" link
+			//----------------------------------------
+			if ( !$direct )
+			{
+				$backURL = remove_query_arg('album');
+				$backURL = remove_query_arg('kpap', $backURL);
+				print "<div id='kpg-backLink'><a href='$backURL'>&laquo; Back to album list</a></div>";
+			}
+
+			//----------------------------------------
+			// Display album information
+			//----------------------------------------
+			$albumTitle    = wp_specialchars( (string) $xml->title );
+			$albumSummary  = wp_specialchars( (string) $xml->subtitle );
+			$albumLocation = wp_specialchars( (string) $xml->gphoto_location );
+			$albumNbPhotos = (string) $xml->gphoto_numphotos;
+
+			print '<div id="kpg-album-description">';
+			print "<div id='kpg-title'>$albumTitle</div>";
+			if ( strlen($albumSummary) )
+			{
+				print "<div id='kpg-summary'>$albumSummary</div>";
+			}
+			if ( strlen($albumLocation) )
+			{
+				print "<div id='kpg-location'>$albumLocation</div>";
+			}
+			print "<div id='kpg-nbPhotos'>$albumNbPhotos photo".($albumNbPhotos > 1 ? 's' : '').'</div>';
+			print '</div>';
+
+			//----------------------------------------
+			// Prepare Highslide if needed
+			//----------------------------------------
+			if ( $this->picEngine == 'highslide' )
+			{
+				print '<div id="controlbar" class="highslide-overlay controlbar">';
+				print '<a href="#" class="previous" onclick="return hs.previous(this)" title="Previous (left arrow key)"></a>';
+				print '<a href="#" class="next" onclick="return hs.next(this)" title="Next (right arrow key)"></a>';
+				print '<a href="#" class="highslide-move" onclick="return false" title="Click and drag to move"></a>';
+				print '<a href="#" class="close" onclick="return hs.close(this)" title="Close"></a>';
+				print '</div>';
+			}
+
+			//----------------------------------------
+			// Prepare some variables
+			//----------------------------------------
+			$page = isset($_GET['kpap']) && intval($_GET['kpap']) > 1 ? intval($_GET['kpap']) : 1; // kpap = kPicasa Album Page
+
+			if ( $this->nbPhotosPerPage > 0 )
+			{
+				$start = ($page - 1) * $this->nbPhotosPerPage;
+				$stop  = $start + $this->nbPhotosPerPage - 1;
+			}
+			else
+			{
+				$start = 0;
+				$stop = count( $xml->entry ) - 1;
+			}
+
+			$thumbIndex = 1; // 144px
+			if ( $this->photoThumbSize == 72 )
+			{
+				$thumbIndex = 0;
+			}
+			elseif ( $this->photoThumbSize == 288 )
+			{
+				$thumbIndex = 2;
+			}
+
+			//----------------------------------------
+			// Loop through the pictures
+			//----------------------------------------
+			print '<table cellpadding="0" cellspacing="0" border="0" width="100%" id="kpg-pictures">';
+			$i = 0; $j = 0;
+			foreach( $xml->entry as $photo )
+			{
+				if ($i >= $start && $i <= $stop)
+				{
+					if ($j % $this->nbPhotosPerRow == 0)
+					{
+						$remainingWidth = 100;
+						if ($j > 0)
+						{
+							print '</tr>';
+						}
+						print '<tr>';
+					}
+
+					// if last cell, simply put remaining width
+					$width = ( ($j+1) % $this->nbPhotosPerRow == 0 ) ? $remainingWidth : round( 100 / $this->nbPhotosPerRow );
+					$remainingWidth -= $width;
+					print "<td width='$width%'>";
+
+					$summary  = wp_specialchars( (string) $photo->summary );
+					$thumbURL = (string) $photo->media_group->media_thumbnail[$thumbIndex]['url'];
+					$thumbH   = (string) $photo->media_group->media_thumbnail[$thumbIndex]['height'];
+					$thumbW   = (string) $photo->media_group->media_thumbnail[$thumbIndex]['width'];
+					$fullURL  = str_replace('/s144/', '/s800/', $thumbURL);
+
+					if ( $this->picEngine == 'lightbox' )
+					{
+						if ( strlen($summary) )
+						{
+							print "<a href='$fullURL' rel='lightbox[kpicasa_gallery]' title='".str_replace("'", "&#39;", $summary)."'><img src='$thumbURL' height='$thumbH' width='$thumbW' alt='".str_replace("'", "&#39;", $summary)."' class='kpg-thumb' /></a>";
+							print "<div class='kpg-summary'>$summary</div>";
+						}
+						else
+						{
+							print "<a href='$fullURL' rel='lightbox[kpicasa_gallery]'><img src='$thumbURL' height='$thumbH' width='$thumbW' alt='' class='kpg-thumb' /></a>";
+						}
+					}
+					elseif ( $this->picEngine == 'highslide' )
+					{
+						if ( strlen($summary) )
+						{
+							print "<a href='$fullURL' rel='highslide' class='highslide'><img src='$thumbURL' height='$thumbH' width='$thumbW' title='".str_replace("'", "&#39;", $summary)."' alt='".str_replace("'", "&#39;", $summary)."' class='kpg-thumb' /></a>";
+							print "<div class='kpg-summary'>$summary</div>";
+							print "<div class='highslide-caption'>$summary</div>";
+						}
+						else
+						{
+							print "<a href='$fullURL' rel='highslide' class='highslide'><img src='$thumbURL' height='$thumbH' width='$thumbW' alt='' class='kpg-thumb' /></a>";
+						}
+					}
+					else
+					{
+						if ( strlen($summary) )
+						{
+							print "<a href='$fullURL' title='".str_replace("'", "&#39;", $summary)."'><img src='$thumbURL' height='$thumbH' width='$thumbW' alt='".str_replace("'", "&#39;", $summary)."' class='kpg-thumb' /></a>";
+							print "<div class='kpg-summary'>$summary</div>";
+						}
+						else
+						{
+							print "<a href='$fullURL'><img src='$thumbURL' height='$thumbH' width='$thumbW' alt='' class='kpg-thumb' /></a>";
+						}
+					}
+					print '</td>';
+					$j++;
+				}
+				$i++;
+			}
+
+			// never leave the last row with insuficient cells
+			if ($this->nbPhotosPerRow > 0)
+			{
+				while ($j % $this->nbPhotosPerRow > 0)
+				{
+					print '<td>&nbsp;</td>';
+					$j++;
+				}
+			}
+
+			print '</tr>';
+			print '</table>';
+			print '<div style="clear: both;" />&nbsp;</div>';
+
+			//----------------------------------------
+			// Paginator
+			//----------------------------------------
+			$extraArgs = array('album' => $album);
+			if (isset($_GET['kpgp']) && intval($_GET['kpgp']) > 1)
+			{
+				$extraArgs['kpgp'] = intval($_GET['kpgp']);
+			}
+			$this->paginator( $page, 'kpap', $this->nbPhotosPerPage, count($xml->entry), $extraArgs );
+			return true;
+		}
+
+		private function paginator ($page, $argName, $perPage, $nbItems, $extraArgs = array())
+		{
+			if ($perPage > 0)
+			{
+				$nbPage = ceil( $nbItems / $perPage );
+				if ($nbPage > 1)
+				{
+					$url = get_permalink();
+					foreach($extraArgs as $key => $value)
+					{
+						$url = add_query_arg($key, $value, $url);
+					}
+
+					print '<div id="kpg-paginator">Page:&nbsp;&nbsp;';
+					for($i = 1; $i <= $nbPage; $i++)
+					{
+						$pageUrl = add_query_arg($argName, $i, $url);
+						if ($i == $page)
+						{
+							print " <span class='kpg-on'>$i</span>";
+						}
+						else
+						{
+							print " <a href='$pageUrl'>$i</a>";
+						}
+					}
+					print '</div>';
+				}
+			}
+		}
+
+		private function fetch( $url )
+		{
+			$fopen_failed = false;
+			$curl_failed  = false;
+
+			// todo: add timeouts?
+
+			if ( ini_get('allow_url_fopen') == '1' )
+			{
+				$data = @file_get_contents($url);
+				if ($data !== false)
+				{
+					return $data;
+				}
+				$fopen_failed = true;
+			}
+			if ( function_exists('curl_init') )
+			{
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				$data = curl_exec($ch);
+				curl_close($ch);
+				if ($data !== false)
+				{
+					return $data;
+				}
+				$curl_failed = true;
+			}
+
+			if ($fopen_failed && $curl_failed)
+			{
+				return new WP_Error( 'kpicasa_gallery-cant-open-url', __("<strong>Error:</strong> your PHP configuration reports that it allows kPicasa Gallery to connect to Picasa Web Albums, but in fact it seems your web host is blocking outgoing requests. Please ask your administrator to allow outgoing requests via file_get_contents() or cURL.") );
+			}
+			elseif ($fopen_failed)
+			{
+				return new WP_Error( 'kpicasa_gallery-cant-open-url', __("<strong>Error:</strong> kPicasa Gallery tried to connect to Picasa Web Albums using file_get_contents() and failed. Your web host is probably blocking outgoing requests.") );
+			}
+			elseif ($curl_failed)
+			{
+				return new WP_Error( 'kpicasa_gallery-cant-open-url', __("<strong>Error:</strong> kPicasa Gallery tried to connect to Picasa Web Albums using cURL and failed. Your web host is probably blocking outgoing requests.") );
+			}
+			else
+			{
+				return new WP_Error( 'kpicasa_gallery-cant-open-url', __("<strong>Error:</strong> your PHP configuration does not allow kPicasa Gallery to connect to Picasa Web Albums. Please ask your administrator to enable allow_url_fopen or cURL.") );
+			}
+		}
+
+		private function checkError( $error )
+		{
+			if ( is_wp_error($error) )
+			{
+				//wp_die( $error, 'kPicasa Gallery Error' );
+				foreach( $error->get_error_messages() as $message )
+				{
+					print $message;
+				}
+				print '<br /><br />';
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== kPicasa Gallery ===
+Tags: picasa, gallery, photo
+Requires at least: 2.2
+Tested up to: 2.7.1
+Stable tag: 0.1.7
+
+Display your Picasa Web Galleries in a post or in a page.
+
+== Description ==
+
+This plugin displays your Picasa Web Galleries in a post or in a page simply by
+creating a post or a page with a special keyword. All the images are kept on the
+Picasa Web Gallery server.
+
+**Please note that PHP5 is required.**
+
+If enabled and properly configured, kPicasa Gallery will use the WP-Cache
+mechanism.
+
+You have to choice of using either Lightbox or Highslide for displaying the
+large version of your photos.
+
+== Installation ==
+
+1. Unzip the archive to your `wp-content/plugins/` folder.
+2. Activate the plugin through the `Plugins` menu in WordPress
+3. Go in the `Options` menu and select `kPicasa Gallery`. Set your parameters.
+4. Create post or a page with `KPICASA_GALLERY` as the only content.
+5. Browse to that post or page, voilà!
+
+== Advanced usage ==
+
+== Show a single ablum ==
+Optionally, you can decide to only display specific albums. For this, you
+will have to go to the Picasa Web Albums and log into your account. You will
+then need to find the internal album name, as explained below.
+
+As an example, for an album named "Trip to Europe", the URL of that album
+will probably look like this: `http://picasaweb.google.com/YourPicasaUsername/TripToEurope`.
+The internal album name is the last portion (`TripToEurope`). You will need to
+find the internal album name for every album you want to display.
+
+You will then need to call kPicasa Gallery like this (this will show 3 specific albums):
+`KPICASA_GALLERY(TripToEurope, TripToAsia, TripToAustralia)`
+
+== Show a single private album ==
+You can also list a private album. For this, you will need its `authkey`.
+You will find it in the URL of your private album on the Picasa Web Albums website.
+
+If the URL to your private album is: `http://picasaweb.google.com/ghebert/TripToEurope?authkey=Gv1sRgCILA9ebdxLyZaQ`.
+You can see the authkey (20 characters long) is the last portion of the URL.
+Be careful not to include the # character that sometimes appear at the end of the URL.
+
+Then you will have to use the following syntax:
+`KPICASA_GALLERY(TripToEurope#Gv1sRgCILA9ebdxLyZaQ)`
+
+**Please note that this feature only works for single albums.**
