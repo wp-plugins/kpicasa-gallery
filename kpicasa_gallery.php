5b13d5b8d297db161ba9c112edcd98aa1d7399d0
kag <kag@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-06 10:28:48 +0000

    Version 0.2.2
    git-svn-id: https://plugins.svn.wordpress.org/kpicasa-gallery/tags/0.2.2@160888 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kpicasa_gallery.php
@@ -0,0 +1,247 @@
+<?php
+/*
+Plugin Name: kPicasa Gallery
+Plugin URI: http://www.boloxe.com/techblog/
+Description: Display your Picasa Web Galleries in a post or in a page.
+Version: 0.2.2
+Author: Guillaume Hébert
+
+Version History
+---------------------------------------------------------------------------
+2007-07-14	0.0.1		First release
+2007-08-23	0.0.2		Bug fix (conflicted with TinyMCE)
+2007-12-07	0.0.3		More robust error handling (no new features)
+2007-12-13	0.0.4		If allow_url_fopen is not enabled, will now try cURL
+2008-01-05	0.0.5		Added UTF-8 support, hopefully better detection of PHP4
+						(instead of crashing), optional pagination, and optional
+						selection of specific albums to be displayed.
+2007-01-12	0.1.0		Changed the way kPicasa is called: created an
+						configuration page in "Admin -> Options" where the
+						parameters are set, hoping it is less confusing for new
+						users. Corrected a bug that affected those who chose to
+						display their album in a post (instead of a page). Added
+						the option to use Lightbox or Highslide.
+2007-01-25	0.1.1		Fixed the error messages. Fixed a pagination bug. Moved
+						the inline styles in favor of a CSS file. Commented the
+						code. Added the option to use no full-size picture
+						engine (for those running their own). Added album
+						descriptions. This fixes almost all the requests I
+						received.
+2007-01-25	0.1.2		Fixed a bug where kPicasa was sending content and
+						breaking PHP redirections.
+2007-01-25	0.1.3		Improved error messages. Easier to visually customize
+						via a CSS file, instead of inline styling. Ability to
+						display galleries from more than one Picasa account,
+						each in a different page/post. New options to change the
+						number of albums to display per line, and the number of
+						pictures per line. Now displays the album description if
+						it exists. It is now possible to not use any engine to
+						display the full-sized picture.
+2007-02-13	0.1.4		Improved the routine to connect to Picasa. Some users
+						reported errors since 0.1.0.
+2007-07-21	0.1.5		Compatible with Wordpress 2.6.0. Updated the look and
+						feel of the configuration page.
+2008-04-04	0.1.6		Compatible with Wordpress 2.7.x. Can now show individual
+						unlisted albums. Can now select thumbnail sizes. Text
+						can now be typed around the album.
+2009-04-05	0.1.7		Fixed a nasty bug from 0.1.6
+2009-04-06	0.1.8		Refreshed Javascript libraries
+2009-04-07	0.1.9		Added Slimbox, Thickbox, fixed a problem with Highslide.
+						Also fixed a bug where a selecting anything but a 144px
+						picture thumbnail would make a picture of the wrong size
+						appear in the javascript popup
+2009-09-01	0.2.0		Added video support. Integrated with Highslide. Other
+						engines are opening a new window to the Picasa
+						original URL
+2009-09-16	0.2.1		Maintenance release. Fixed a CSS annoyance. Now using
+						WP_PLUGIN_URL if it's defined (WP 2.6+)
+2009-10-06	0.2.2		In an attempt to resolve a conflict with the "Google
+						Analytics for WordPress" plugin, kPicasa is now loaded
+						earlier (priority 9 instead of 10). Added dirname()
+						to the kpg.class.php require_once() call.
+
+Licence
+---------------------------------------------------------------------------
+    Copyright 2007, 2008, 2009  Guillaume Hébert  (email : kag@boloxe.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+if ( version_compare(PHP_VERSION, '5.0.0', '<') )
+{
+	print 'kPicasa Gallery requires PHP version 5 or greater. You are running PHP version '.PHP_VERSION;
+	exit;
+}
+if ( !defined('KPICASA_GALLERY_DIR') )
+{
+	if ( defined('WP_PLUGIN_URL') )
+	{
+		define('KPICASA_GALLERY_DIR', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)));
+	}
+	else
+	{
+		define('KPICASA_GALLERY_DIR', get_bloginfo('wpurl').'/wp-content/plugins/'.dirname(plugin_basename(__FILE__)));
+	}
+}
+if ( !defined('KPICASA_GALLERY_FILTER_PRIORITY') )
+{
+	define('KPICASA_GALLERY_FILTER_PRIORITY', 9);
+}
+
+$kpg_picEngine = get_option( 'kpg_picEngine' );
+
+if ( function_exists('is_admin') )
+{
+	if ( !is_admin() )
+	{
+		if ( function_exists('add_action') )
+		{
+			add_action('wp_head', 'initKPicasaGallery');
+		}
+		if ( function_exists('add_filter') )
+		{
+			add_filter('the_content', 'loadKPicasaGallery', KPICASA_GALLERY_FILTER_PRIORITY);
+		}
+
+		if ( function_exists('wp_enqueue_script') )
+		{
+			if ( $kpg_picEngine == 'highslide' )
+			{
+				wp_enqueue_script('highslide', KPICASA_GALLERY_DIR.'/highslide/highslide.js', array(), '4.1.2');
+			}
+			elseif ( $kpg_picEngine == 'lightbox' )
+			{
+				wp_enqueue_script('lightbox2', KPICASA_GALLERY_DIR.'/lightbox2/js/lightbox.js', array('prototype', 'scriptaculous-effects', 'scriptaculous-builder'), '2.04');
+			}
+			elseif ( $kpg_picEngine == 'slimbox2' )
+			{
+				wp_enqueue_script('slimbox2', KPICASA_GALLERY_DIR.'/slimbox2/js/slimbox2.js', array('jquery'), '2.02');
+			}
+			elseif ( $kpg_picEngine == 'thickbox' )
+			{
+				wp_enqueue_script('thickbox');
+			}
+		}
+	}
+	else
+	{
+		if ( function_exists('add_action') )
+		{
+			add_action('admin_menu', 'adminKPicasaGallery');
+		}
+	}
+}
+
+function initKPicasaGallery()
+{
+	global $kpg_picEngine;
+
+	print "<link rel='stylesheet' href='".KPICASA_GALLERY_DIR."/kpicasa_gallery.css' type='text/css' media='screen' />\n";
+
+	if ( $kpg_picEngine == 'highslide' )
+	{
+		$picEngineDir = KPICASA_GALLERY_DIR.'/highslide';
+		print "<link rel='stylesheet' href='$picEngineDir/highslide.css' type='text/css' media='screen' />\n";
+		print "<link rel='stylesheet' href='$picEngineDir/kpicasa.css' type='text/css' media='screen' />\n";
+		print "<!--[if lt IE 7]>\n";
+		print "<link rel='stylesheet' type='text/css' href='$picEngineDir/highslide-styles-ie6.css' />\n";
+		print "<![endif]-->\n";
+
+		print "<script type='text/javascript' src='".KPICASA_GALLERY_DIR."/swfobject.js'></script>\n";
+
+		print "<script type='text/javascript'>\n";
+		print "	hs.graphicsDir      = '$picEngineDir/graphics/';\n";
+		print "	hs.showCredits      = false;\n";
+		print "	hs.outlineType      = 'rounded-white';\n";
+		print "	hs.wrapperClassName = 'highslide-white';\n";
+		print "	if (hs.registerOverlay) {\n";
+			print "		hs.registerOverlay({ thumbnailId: null, overlayId: 'controlbar', position: 'top right', hideOnMouseOut: true });\n";
+		print "	}\n";
+		print "</script>\n";
+	}
+	elseif ( $kpg_picEngine == 'lightbox' )
+	{
+		$picEngineDir = KPICASA_GALLERY_DIR.'/lightbox2';
+		print "<link rel='stylesheet' href='$picEngineDir/css/lightbox.css' type='text/css' media='screen' />\n";
+
+		print "<script type='text/javascript'>\n";
+		print "	LightboxOptions.fileLoadingImage        = '$picEngineDir/images/loading.gif';\n";
+		print "	LightboxOptions.fileBottomNavCloseImage = '$picEngineDir/images/closelabel.gif';\n";
+		print "</script>\n";
+	}
+	elseif ( $kpg_picEngine == 'slimbox2' )
+	{
+		$picEngineDir = KPICASA_GALLERY_DIR.'/slimbox2';
+		print "<link rel='stylesheet' href='$picEngineDir/css/slimbox2.css' type='text/css' media='screen' />\n";
+	}
+	elseif ( $kpg_picEngine == 'thickbox' )
+	{
+		$picEngineDir = KPICASA_GALLERY_DIR.'/thickbox';
+		$picEngineDir = "http://www.boloxe.com/blog/wp-includes/js/thickbox";
+		print "<link rel='stylesheet' href='$picEngineDir/thickbox.css' type='text/css' media='screen' />\n";
+	}
+}
+
+function loadKPicasaGallery ( $content = '' )
+{
+	$tmp = strip_tags(trim($content));
+	//$regex = '/^KPICASA_GALLERY[\s]*(\(.*\))?$/';
+	$regex = '/^[\s]*KPICASA_GALLERY[\s]*(\(.*\))?[\s]*$/m';
+
+	if ( preg_match($regex, $tmp, $matches) )
+	{
+		$showOnlyAlbums = array();
+		$username       = null;
+
+		if ( isset($matches[1]) )
+		{
+			$args = explode(',', substr( substr($matches[1], 0, strlen($matches[1])-1), 1 ));
+			if ( count($args) > 0 )
+			{
+				foreach( $args as $value )
+				{
+					$value = str_replace(' ', '', $value);
+					if ($username == null && 'username:' == substr($value, 0, 9) && strlen($value) > 9)
+					{
+						$username = substr($value, 9);
+					}
+					else
+					{
+						$showOnlyAlbums[] = $value;
+					}
+				}
+			}
+		}
+
+		require_once(dirname(__FILE__).'/kpg.class.php');
+
+		ob_start();
+		$gallery = new KPicasaGallery($username, $showOnlyAlbums);
+		$buffer  = ob_get_clean();
+		return str_replace($matches[0], $buffer, $content);
+	}
+
+	return $content;
+}
+
+function adminKPicasaGallery()
+{
+	if ( function_exists('add_options_page') )
+	{
+		add_options_page('kPicasa Gallery Plugin Options', 'kPicasa Gallery', 8, dirname(__FILE__).'/param.php');
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,65 @@
+=== kPicasa Gallery ===
+Tags: picasa, gallery, photo
+Requires at least: 2.2
+Tested up to: 2.8.4
+Stable tag: trunk
+
+Display your Picasa Web Galleries in a post or in a page.
+
+== Description ==
+
+This plugin displays your Picasa Web Galleries in a post or in a page simply by
+creating a post or a page with a special keyword. All the images are kept on the
+Picasa Web Gallery server.
+
+**Please note that PHP5 is required.**
+
+If enabled and properly configured, kPicasa Gallery will use the WP-Cache
+mechanism.
+
+You have to choice of using either Highslide, Lightbox, Slimbox or Thickbox for
+displaying the large version of your photos.
+
+Highslide supports playing videos directly from your site. Selecting another
+engine will open a new window to the original Picasa URL.
+
+== Installation ==
+
+1. Unzip the archive to your `wp-content/plugins/` folder.
+2. Activate the plugin through the `Plugins` menu in WordPress
+3. Go in the `Options` menu, select `kPicasa Gallery` and set your parameters.
+4. Create post or a page with `KPICASA_GALLERY` as the only content.
+5. Browse to that post or page, voilà!
+
+== Advanced usage ==
+
+= Show specific albums =
+Browse to the [Picasa web site](http://picasaweb.google.com/) and log yourself
+in. You will then need to find the internal album name, as explained below.
+
+For an album named "Trip to Europe", the URL of that album will probably look
+like this: `http://picasaweb.google.com/YourPicasaUsername/TripToEurope`.
+The internal album name is the last portion (`TripToEurope`). You will need to
+find the internal album name for every album you want to display.
+
+You will then need to call kPicasa Gallery like this (this will show 3 specific albums):
+`KPICASA_GALLERY(TripToEurope, TripToAsia, TripToAustralia)`
+
+= Show a private album =
+Browse to the [Picasa web site](http://picasaweb.google.com/) and log yourself
+in. You will then need to find the internal album name and its `authkey`, as
+explained below.
+
+Let's say the URL to your private album is: `http://picasaweb.google.com/ghebert/TripToEurope?authkey=Gv1sRgCILA9ebdxLyZaQ`.
+The `authkey` is the last portion of the URL and is always 20 characters long.
+Be careful not to include the # character that sometimes appear at the end of that URL.
+
+You would have to use the following syntax:
+`KPICASA_GALLERY(TripToEurope#Gv1sRgCILA9ebdxLyZaQ)`
+
+= Show albums from an alternative Picasa account =
+If you have more than one Picasa account, you can show the albums from another
+account by calling kPicasa Gallery like this:
+`KPICASA_GALLERY(username:YourOtherUsername)`
+
+**Please note that you can't combine multiple accounts into the same post or page.**
